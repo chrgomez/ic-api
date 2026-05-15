470600c80ee36035282bc303fa6ec01a964b085c
Christian <gomezchristian.a@gmail.com>
2026-05-15 17:43:48 -0300

    feat: se agrea test

---
+++ src/test/java/com/utn/project/ci/ProjectCiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.utn.project.ci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
