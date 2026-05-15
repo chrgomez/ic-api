522fef503563ca0c84234b1592f2476693184235
Christian <gomezchristian.a@gmail.com>
2026-05-15 06:22:01 -0300

    Commit inicial: Proyecto base

---
+++ src/main/java/com/utn/project/ci/ProjectCi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.utn.project.ci;
+
+/**
+ *
+ * @author christian
+ */
+public class ProjectCi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
