1bc94630421e64c26a03d0e961f31aa5a3b66816
Christian <gomezchristian.a@gmail.com>
2026-06-09 12:44:48 -0300

    Probando demostracion fallida 1

--- src/main/java/com/utn/project/ci/ProjectCi.java
+++ src/main/java/com/utn/project/ci/ProjectCi.java
@@ -8,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjectCi {
     
+    private final String version = "1.0.0";
+    
     public static void main(String[] args) {
         SpringApplication.run(ProjectCi.class, args);
         
